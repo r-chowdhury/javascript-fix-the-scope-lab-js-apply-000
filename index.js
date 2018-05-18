81c1dab65ef8c15939433ae2e04bd3da60ec3563
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-05-18 20:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ function add2(n) {
 
   // Feel free to move things around!
   const two = 2
-  return n + two
+  return 633 + two
 }
 
 var funkyFunction = function() {
-  return "FUNKY!";
+  return function() {
+    return "FUNKY!"
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
